e3431dfe305221a24b73bf62eec08a0b47e94680
AlanGuo <AlanGuo>
2025-07-02 15:28:34 +0800

    connection pool

--- main.js
+++ main.js
@@ -5,4 +5,20 @@ const PineIndicator = require('./src/classes/PineIndicator');
 const PinePermManager = require('./src/classes/PinePermManager');
 
 const TradingView = { ...miscRequests, Client, BuiltInIndicator, PineIndicator, PinePermManager };
-export { TradingView }
+
+// CommonJS 导出 (向后兼容)
+module.exports = TradingView;
+
+// ES6 模块导出
+export { Client, BuiltInIndicator, PineIndicator, PinePermManager };
+export default TradingView;
+
+// 导出 miscRequests 中的所有函数
+export const {
+  searchMarket,
+  searchIndicator,
+  getIndicator,
+  getPrivateIndicators,
+  getUser,
+  scanSymbol
+} = miscRequests;
